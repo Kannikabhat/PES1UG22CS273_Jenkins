8214b9bf2338b42b58b2f199558945e8a22ef29a
Kannikabhat <115685552+Kannikabhat@users.noreply.github.com>
2025-03-13 16:08:13 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(void) {
   cout << "Hello, World\n";
-  cout << "Hello, KANNIKA \n";
+  cout << "Hello, KANNIKA g \n";
   cout << "I have successfully built and run this  \n";
   return 0;
 }
